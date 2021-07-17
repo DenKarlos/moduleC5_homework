f7c467d6b1dc89c42cf57f3dff82e8fa39fb17ed
Denis <kominaretsdenis@gmail.com>
2021-07-17 18:32:15 +0600

    first commit

---
+++ С5_2_JSON_XML/parseJSON.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const jsonString = `
+{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+
+
+   const data = JSON.parse(jsonString);
+
+   const resObj = {
+    list: [
+        {
+            name: data.list[0].name,
+            age: Number(data.list[0].age),
+            prof: data.list[0].prof
+        },
+        {
+            name: data.list[1].name,
+            age: Number(data.list[1].age),
+            prof: data.list[1].prof
+        }
+    ]
+};
+
+console.log(resObj); 
